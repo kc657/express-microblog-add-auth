863d8cd13d8365ae29b6641094a65387097bdb0e
kc657 <kvn.0218@gmail.com>
2017-08-10 11:18:51 -0700

    added login and signup

--- server.js
+++ server.js
@@ -1,55 +1,82 @@
 // require express and other modules
 var express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser'),
-    mongoose = require('mongoose'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
 
-    //  NEW ADDITIONS
-    cookieParser = require('cookie-parser'),
-    session = require('express-session'),
-    passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
+  //  NEW ADDITIONS
+  cookieParser = require('cookie-parser'),
+  session = require('express-session'),
+  passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy
+
+// require Post model
+var db = require('./models'),
+  Post = db.Post
+User = db.User
 
 // configure bodyParser (for receiving form data)
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 // serve static files from public folder
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
 // middleware for auth
-app.use(cookieParser());
+app.use(cookieParser())
 app.use(session({
   secret: 'supersecretkey', // change this!
   resave: false,
   saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 // passport config
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 // set view engine to hbs (handlebars)
-app.set('view engine', 'hbs');
-
-// require Post model
-var db = require('./models'),
-    Post = db.Post;
-    User = db.User;
-
+app.set('view engine', 'hbs')
 
 // HOMEPAGE ROUTE
 
 app.get('/', function (req, res) {
-  res.render('index');
-});
+  res.render('index')
+})
 
 // show signup view
 app.get('/signup', function (req, res) {
- res.sendFile('/views/signup.html', { root: __dirname });
+  res.sendFile('/views/signup.html', {
+    root: __dirname
+  })
+})
+// AUTH ROUTES
+
+// sign up new user, then log them in
+// hashes and salts password, saves new user to db
+app.post('/signup', function (req, res) {
+  User.register(new User({ username: req.body.username }), req.body.password,
+    function (err, newUser) {
+      passport.authenticate('local')(req, res, function () {
+        res.redirect('/')
+      })
+    }
+  )
+})
+
+// show login view
+app.get('/login', function (req, res) {
+ res.sendFile('/views/login.html', { root: __dirname });
+});
+
+// log in user
+app.post('/login', passport.authenticate('local'), function (req, res) {
+  console.log(req.user);
+  res.send('logged in!!!'); // sanity check
+  // res.redirect('/'); // preferred!
 });
 
 // API ROUTES
@@ -59,90 +86,111 @@ app.get('/api/posts', function (req, res) {
   // find all posts in db
   Post.find(function (err, allPosts) {
     if (err) {
-      res.status(500).json({ error: err.message });
+      res.status(500).json({
+        error: err.message
+      })
     } else {
-      res.json({ posts: allPosts });
+      res.json({
+        posts: allPosts
+      })
     }
-  });
-});
+  })
+})
 
 // create new post
 app.post('/api/posts', function (req, res) {
   // create new post with form data (`req.body`)
-  var newPost = new Post(req.body);
+  var newPost = new Post(req.body)
 
   // save new post in db
   newPost.save(function (err, savedPost) {
     if (err) {
-      res.status(500).json({ error: err.message });
+      res.status(500).json({
+        error: err.message
+      })
     } else {
-      res.json(savedPost);
+      res.json(savedPost)
     }
-  });
-});
+  })
+})
 
 // get one post
 app.get('/api/posts/:id', function (req, res) {
   // get post id from url params (`req.params`)
-  var postId = req.params.id;
+  var postId = req.params.id
 
   // find post in db by id
-  Post.findOne({ _id: postId }, function (err, foundPost) {
+  Post.findOne({
+    _id: postId
+  }, function (err, foundPost) {
     if (err) {
-      if (err.name === "CastError") {
-        res.status(404).json({ error: "Nothing found by this ID." });
+      if (err.name === 'CastError') {
+        res.status(404).json({
+          error: 'Nothing found by this ID.'
+        })
       } else {
-        res.status(500).json({ error: err.message });
+        res.status(500).json({
+          error: err.message
+        })
       }
     } else {
-      res.json(foundPost);
+      res.json(foundPost)
     }
-  });
-});
+  })
+})
 
 // update post
 app.put('/api/posts/:id', function (req, res) {
   // get post id from url params (`req.params`)
-  var postId = req.params.id;
+  var postId = req.params.id
 
   // find post in db by id
-  Post.findOne({ _id: postId }, function (err, foundPost) {
+  Post.findOne({
+    _id: postId
+  }, function (err, foundPost) {
     if (err) {
-      res.status(500).json({ error: err.message });
+      res.status(500).json({
+        error: err.message
+      })
     } else {
       // update the posts's attributes
-      foundPost.title = req.body.title;
-      foundPost.description = req.body.description;
+      foundPost.title = req.body.title
+      foundPost.description = req.body.description
 
       // save updated post in db
       foundPost.save(function (err, savedPost) {
         if (err) {
-          res.status(500).json({ error: err.message });
+          res.status(500).json({
+            error: err.message
+          })
         } else {
-          res.json(savedPost);
+          res.json(savedPost)
         }
-      });
+      })
     }
-  });
-});
+  })
+})
 
 // delete post
 app.delete('/api/posts/:id', function (req, res) {
   // get post id from url params (`req.params`)
-  var postId = req.params.id;
+  var postId = req.params.id
 
   // find post in db by id and remove
-  Post.findOneAndRemove({ _id: postId }, function (err, deletedPost) {
+  Post.findOneAndRemove({
+    _id: postId
+  }, function (err, deletedPost) {
     if (err) {
-      res.status(500).json({ error: err.message });
+      res.status(500).json({
+        error: err.message
+      })
     } else {
-      res.json(deletedPost);
+      res.json(deletedPost)
     }
-  });
-});
-
+  })
+})
 
 // listen on port 3000
-app.listen(3000, function() {
-  console.log('server started');
-});
+app.listen(3000, function () {
+  console.log('server started')
+})
